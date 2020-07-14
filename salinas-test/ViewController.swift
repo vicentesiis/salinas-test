f2ef506595869a0a29472d96391ae91ea1287018
Vicente Cantu <vice.00@hotmail.com>
2020-07-14 16:51:30 -0500

    Initial Commit

---
+++ salinas-test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  salinas-test
+//
+//  Created by Macintosh HD on 14/07/20.
+//  Copyright © 2020 vicentesiis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
